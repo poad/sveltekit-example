dab25a0ed3da3f1857fa6d92e3f0b99a8ce05a17
Kenji Saito <ken-yo@mbr.nifty.com>
2022-03-07 13:38:20 +0900

    Fix build error

--- sveltekit-vite-example/src/routes/__layout.svelte
+++ sveltekit-vite-example/src/routes/__layout.svelte
@@ -1,48 +1,45 @@
 <script lang="ts">
-  import Header from "$lib/header/Header.svelte";
-  import "../app.postcss";
+	import Header from '$lib/header/Header.svelte';
+	import '../app.css';
 </script>
 
-<Header>
-  <main>
-    <slot />
-  </main>
+<Header />
 
-  <footer>
-    <p>
-      visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit
-    </p>
-  </footer>
+<main>
+	<slot />
+</main>
 
-  <style>
-    main {
-      flex: 1;
-      display: flex;
-      flex-direction: column;
-      padding: 1rem;
-      width: 100%;
-      max-width: 1024px;
-      margin: 0 auto;
-      box-sizing: border-box;
-    }
+<footer>
+	<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+</footer>
 
-    footer {
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-      padding: 40px;
-    }
+<style>
+	main {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		padding: 1rem;
+		width: 100%;
+		max-width: 1024px;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
 
-    footer a {
-      font-weight: bold;
-    }
+	footer {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding: 40px;
+	}
 
-    @media (min-width: 480px) {
-      footer {
-        padding: 40px 0;
-      }
-    }
-  </style>
-</Header>
-<slot />
+	footer a {
+		font-weight: bold;
+	}
+
+	@media (min-width: 480px) {
+		footer {
+			padding: 40px 0;
+		}
+	}
+</style>
